4ff7c1968c484b06b62afc53cf8c7acd963d8f54
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-08 18:42:18 -0700

    Front End

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat'
+import axios from './axios'
+
+const Pusher = require('pusher-js')
+
+function App() {
+  const [messages,setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+          .then(res => {
+            setMessages(res.data)
+          })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('aa5181e832219eea796f', {
+      cluster: 'us3'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+
+  }, [messages])
+
+console.log(messages)
+  return (
+    <div className="app">
+     <div className='app_body'>
+     <Sidebar />
+     <Chat messages={messages} />
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-4SIFhHBvG2e2Lwsp0qdltqnOZx4_Nmk",
+    authDomain: "whatsapp-clone-37db0.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-37db0.firebaseio.com",
+    projectId: "whatsapp-clone-37db0",
+    storageBucket: "whatsapp-clone-37db0.appspot.com",
+    messagingSenderId: "102189799774",
+    appId: "1:102189799774:web:290691c45f262bf79bd7cf"
+  };
